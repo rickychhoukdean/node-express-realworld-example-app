1d07cc02885661576572a748df264a483316fca0
rickychhoukdean <chhoukdean184@msn.com>
2019-12-16 17:18:05 -0500

    Add endpoints for list & feeds

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -98,7 +98,7 @@ router.post("/:article/comments", auth.required, function(req, res, next) {
         return res.sendStatus(401);
       }
 
-      var comment = new Comment(req.body.comment);
+      const comment = new Comment(req.body.comment);
       comment.article = req.article;
       comment.author = user;
 
@@ -176,4 +176,102 @@ router.delete("/:article/comments/:comment", auth.required, function(
   }
 });
 
+router.get("/", auth.optional, function(req, res, next) {
+  const query = {};
+  const limit = 20;
+  const offset = 0;
+
+  if (typeof req.query.limit !== "undefined") {
+    limit = req.query.limit;
+  }
+
+  if (typeof req.query.offset !== "undefined") {
+    offset = req.query.offset;
+  }
+
+  if (typeof req.query.tag !== "undefined") {
+    query.tagList = { $in: [req.query.tag] };
+  }
+  Promise.all([
+    req.query.author ? User.findOne({ username: req.query.author }) : null,
+    req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
+  ])
+    .then(function(results) {
+      const author = results[0];
+      const favoriter = results[1];
+
+      if (author) {
+        query.author = author._id;
+      }
+
+      if (favoriter) {
+        query._id = { $in: favoriter.favorites };
+      } else if (req.query.favorited) {
+        query._id = { $in: [] };
+      }
+
+      return Promise.all([
+        Article.find(query)
+          .limit(Number(limit))
+          .skip(Number(offset))
+          .sort({ createdAt: "desc" })
+          .populate("author")
+          .exec(),
+        Article.count(query).exec(),
+        req.payload ? User.findById(req.payload.id) : null
+      ]).then(function(results) {
+        const articles = results[0];
+        const articlesCount = results[1];
+        const user = results[2];
+
+        return res.json({
+          articles: articles.map(function(article) {
+            return article.toJSONFor(user);
+          }),
+          articlesCount: articlesCount
+        });
+      });
+    })
+    .catch(next);
+});
+
+router.get("/feed", auth.required, function(req, res, next) {
+  const limit = 20;
+  const offset = 0;
+
+  if (typeof req.query.limit !== "undefined") {
+    limit = req.query.limit;
+  }
+
+  if (typeof req.query.offset !== "undefined") {
+    offset = req.query.offset;
+  }
+
+  User.findById(req.payload.id).then(function(user) {
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    Promise.all([
+      Article.find({ author: { $in: user.following } })
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .populate("author")
+        .exec(),
+      Article.count({ author: { $in: user.following } })
+    ])
+      .then(function(results) {
+        const articles = results[0];
+        const articlesCount = results[1];
+
+        return res.json({
+          articles: articles.map(function(article) {
+            return article.toJSONFor(user);
+          }),
+          articlesCount: articlesCount
+        });
+      })
+      .catch(next);
+  });
+});
 module.exports = router;
